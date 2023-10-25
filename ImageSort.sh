ac44e35612a5c2cbee0b4867ffceaafd02193d0a
Di Grande Nicolas <dn300621@luci155024>
2023-10-25 11:14:58 +0200

    V4

--- ImageSort.sh
+++ ImageSort.sh
@@ -4,12 +4,13 @@ copy_image()
 {
     anne=$(exif -t 0x0132 -m $1 | cut -d ':' -f1)  
     annee2=$(exif -t 0x0132 -m $1 | cut -d ' ' -f1)
+    annee3=$(exif -t 0x0132 -m $1 | cut -d ' ' -f1 | tr ':' '-')
     mois=$(exif -t 0x0132 -m $1 | cut -d ':' -f2)
-    heure=$(exif -t 0x0132 -m $1 | cut -d ' ' -f1 | tr ':' '-')
+    heure=$(exif -t 0x0132 -m $1 | cut -d ' ' -f2)
 
     mkdir -p "$2/$anne/$mois"
 
-    cp $1 "$2/$anne/$mois/$heure.jpg"
+    cp $1 "$2/$anne/$mois/${annee3}_$heure.jpg"
 }
 
 mkdir -p "$2"
